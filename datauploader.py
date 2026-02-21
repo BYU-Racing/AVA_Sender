69a3b2d2a685488751856aea277a86af11bd4f93
Ruby-Hunter <rex9832@gmail.com>
2026-02-21 08:41:52 -0700

    Updated uploader

--- datauploader.py
+++ datauploader.py
@@ -15,25 +15,53 @@ assert struct.calcsize(FMT) == 14
 def now_ms() -> int:
     return int(time.monotonic() * 1000) & 0xFFFFFFFF
 
-def make_packet() -> bytes:
+def make_packet(msg_id=0) -> bytes:
     ts = now_ms()
-    msg_id = random.randint(0, 255)
 
-    length = random.randint(0, 8)
+    length = 8
     data = bytearray(8)
-    for i in range(length):
-        data[i] = random.randint(0, 255)
+    match msg_id:
+        case 0:
+            data[0] = random.randint(0, 1)
+        case 1 | 2 | 3:
+            data[0] = random.randint(0, 255)
+            data[1] = random.randint(0, 255)
+        case 4:
+            data[0] = random.randint(0, 5)
+            data[1] = random.randint(0, 255)
+        case 5:
+            data[0] = random.randint(0, 3)
+            for i in range(1, 4):
+                data[i] = random.randint(0, 255)
+        case 6:
+            data[0] = random.randint(0, 3)
+            for i in range(1, 6):
+                data[i] = random.randint(0, 255)
+        case 7: 
+            data[0] = random.randint(0, 100)
+        case 8:
+            data[0] = random.randint(0, 150)
+        case 9: # GPS
+            for i in range(0, 7):
+                data[i] = random.randint(0, 255)
+        case 10:
+            data[0] = random.randint(0, 4)
+
+    # for i in range(length):
+    #     data[i] = random.randint(0+20, 255-200)
 
     return struct.pack(FMT, ts, msg_id, length, bytes(data))
 
 async def main():
+    msg_id = 0
     print(f"Connecting to {WS_URL}")
     async with websockets.connect(WS_URL, ping_interval=20, ping_timeout=20) as ws:
         print("Connected. Sending fake packets (Ctrl+C to stop).")
         while True:
-            pkt = make_packet()
+            pkt = make_packet(msg_id)
             await ws.send(pkt)  # sends as binary frame
             await asyncio.sleep(0.05)  # 20 Hz. change as needed
+            msg_id = msg_id + 1 if msg_id < 10 else 0
 
 if __name__ == "__main__":
     try:
