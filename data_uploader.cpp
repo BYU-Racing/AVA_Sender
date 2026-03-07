87749e196b40a57cbf790703e2f4075a98843879
Ruby-Hunter <rex9832@gmail.com>
2026-03-07 10:48:19 -0700

    fixed id size

--- data_uploader.cpp
+++ data_uploader.cpp
@@ -37,7 +37,7 @@
 */
 struct pi_to_server {
     uint32_t timestamp;
-    uint32_t id;
+    uint8_t id;
     uint8_t length;
     uint8_t bytes[8];
 } __attribute__((packed));
@@ -139,7 +139,7 @@ static inline void print_frame(const struct can_frame& frame, uint32_t raw_id)
 std::string url = "ws://13.58.232.73:8000/api/ws/send";
 const uint64_t RECONNECT_DELAY_MS = 5000; // 5 seconds
 
-static_assert(sizeof(pi_to_server) == 17, "pi_to_server must be 17 bytes"); // Constantly checks that packet is the right size
+static_assert(sizeof(pi_to_server) == 14, "pi_to_server must be 14 bytes"); // Constantly checks that packet is the right size
 
 static int openCANSocket(const char* ifname){
     int s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
