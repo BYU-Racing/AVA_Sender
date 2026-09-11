cf2a2958f1689a562cd7c92c34663adeb0b5fee5
Blake Hill <rex9832@gmail.com>
2026-09-11 14:11:54 -0600

    fixed HZ frequency

--- data_upload_test.py
+++ data_upload_test.py
@@ -25,7 +25,7 @@ URL_PREFIX = "ws://"
 URL_SUFFIX = ":8000/api/ws/send"
 WS_URL = URL_PREFIX + IP + URL_SUFFIX
 
-SEND_FQ_HZ = 100
+SEND_FQ_HZ = 20
 
 # struct pi_to_server:
 # uint32_t timestamp; uint32_t id; uint8_t length; uint8_t bytes[8];
@@ -317,7 +317,7 @@ async def main():
             msg_id = DBC_MESSAGE_IDS[msg_index]
             pkt = make_packet(msg_id)
             await ws.send(pkt)  # sends as binary frame
-            await asyncio.sleep(1 / SEND_FQ_HZ)  # in sec
+            await asyncio.sleep(1 / (SEND_FQ_HZ * len(DBC_MESSAGE_IDS)))  # in sec
             msg_index = (msg_index + 1) % len(DBC_MESSAGE_IDS)
 
 
