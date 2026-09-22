de01c74842f019092ec8cc23cc0b034753c1943a
Blake Hill <rex9832@gmail.com>
2026-09-21 21:30:50 -0600

    Finished RKT test for Raspi

--- gnss/gnss_rtk_test.py
+++ gnss/gnss_rtk_test.py
@@ -1,4 +1,4 @@
-# Filename: gnss_base_station.py
+# Filename: gnss_rtk_test.py
 # Author:   Blake Hill
 
 import threading
@@ -11,6 +11,7 @@ from pyubx2 import (
     UBXReader,
 )
 from serial import Serial
+from websockets.exceptions import ConnectionClosed
 from websockets.sync.client import connect
 
 port = "/dev/ttyACM0"
@@ -24,7 +25,6 @@ ws_url = WS_PREFIX + ws_ip + WS_SUFFIX
 ser = Serial(port, baud, timeout=1)
 ubx = UBXReader(ser, protfilter=UBX_PROTOCOL)
 
-ws = connect(ws_url)
 stop_event = threading.Event()
 
 
@@ -33,8 +33,8 @@ def config_ubx():
         # Set normal mode
         ("CFG_TMODE_MODE", 0),
         # UBX and RTCM3 protocol over USB
-        ("CFG_USBOUTPROT_UBX", 1),  # Enable UBX protocol over USB
-        ("CFG_USBOUTPROT_RTCM3X", 1),  # Enable RTCM3 protocol over USB
+        ("CFG_USBINPROT_UBX", 1),  # Enable UBX protocol over USB
+        ("CFG_USBINPROT_RTCM3X", 1),  # Enable RTCM3 protocol over USB
         # Allox UBX results out over USB
         ("CFG_USBOUTPROT_UBX", 1),
         # Output corrected pos and RTK status
@@ -43,14 +43,23 @@ def config_ubx():
         ("CFG_MSGOUT_UBX_RXM_RTCM_USB", 1),
     ]
 
-    command = UBXMessage(SET_LAYER_RAM, TXN_NONE, config)
+    command = UBXMessage.config_set(SET_LAYER_RAM, TXN_NONE, config)
     ser.write(command.serialize())
     ser.flush()
 
 
-def read_from_ubx() -> None:
+# Called in a separate thread
+def read_from_ubx(ubx: UBXReader, stop_event: threading.Event) -> None:
     # carrSoln = carrier-range solution status
     solution_status = {0: "No RTK", 1: "RTK float", 2: "RTK fixed"}
+    fix_types = {
+        0: "No fix",
+        1: "Dead reckoning",
+        2: "2D fix",
+        3: "3D fix",
+        4: "GNSS + dead reckoning",
+        5: "Time-only fix",
+    }
     while not stop_event.is_set():
         raw, msg = ubx.read()
 
@@ -58,5 +67,76 @@ def read_from_ubx() -> None:
             continue
 
         if msg.identity == "NAV-PVT":
-            solution = solution_status.get(msg.carrSoln, f"Unknown ({msg.carrSoln})")
-            print(f"")
+            solu = solution_status.get(msg.carrSoln, f"Unknown ({msg.carrSoln})")
+            fix = fix_types.get(msg.fixType, f"Unknown ({msg.fixType})")
+            pos_info = (
+                f"Lat: {msg.lat} | Lon: {msg.lon} | Acc: {msg.hAcc} | "
+                f"Elev: {msg.hMSL / 1000}m\n"
+                if not msg.invalidLlh
+                else "Pos Invalid\n"
+            )
+            dir_info = f"Speed: {msg.gSpeed / 1000}m/s | Heading: {msg.headMot}deg\n\n"
+            print(
+                f"{solu} | DiffCorrections: {bool(msg.diffSoln)} | "
+                f"FixOK: {msg.gnssFixOk} | Fix Type: {fix} | NumSat: {msg.numSV}\n"
+                + pos_info
+                + dir_info,
+                flush=True,
+            )
+        elif msg.identity == "RXM-RTCM":
+            rtcm_usage = {
+                0: "Unknown",
+                1: "Not used",
+                2: "Used successfully",
+            }
+            usage = rtcm_usage.get(msg.msgUsed, f"Unknown ({msg.msgUsed})")
+            print(
+                f"RTCM recv from read: type={msg.msgType}, "
+                f"used={usage}, CRC fail?={bool(msg.crcFailed)}",
+                flush=True,
+            )
+
+
+def main() -> None:
+    config_ubx()
+    reader_thread = threading.Thread(
+        target=read_from_ubx, args=(ubx, stop_event), daemon=True
+    )
+    reader_thread.start()
+    try:
+        while not stop_event.is_set():
+            try:
+                with connect(ws_url) as ws:
+                    print("Connected to Base Station WS")
+                    for data in ws:
+                        if stop_event.is_set():
+                            break
+
+                        if not isinstance(data, bytes):
+                            print("Ignoring non-binary WS msg")
+                            continue
+
+                        ser.write(data)
+
+            except ConnectionClosed as err:
+                print(f"WS Disconnected: {err}")
+            except OSError as err:
+                print(f"Couldn't connect: {err}")
+
+            if not stop_event.is_set():
+                print("Reconnecting to WS in 1s")
+                stop_event.wait(1)
+                continue
+    finally:
+        print("Closing WS")
+        stop_event.set()
+        reader_thread.join(timeout=2)
+        ser.close()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        stop_event.set()
+        print("\nStopped by Ctrl+C")
